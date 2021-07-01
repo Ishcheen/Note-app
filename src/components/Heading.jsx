df08b28edbbe59be5e32aa9f00bc998f8293e9d1
Ishcheen <72345411+Ishcheen@users.noreply.github.com>
2021-07-01 20:07:35 +0530

    Add files via upload

---
+++ src/components/Heading.jsx
@@ -0,0 +1,12 @@
+import React from "E:/keeper-app-part-1/node_modules/react";
+
+function Heading() {
+    const header=(
+         <header>
+            <h1>Keeper</h1> 
+         </header>
+    )
+    return header;
+}
+
+export default Heading;
